65ed5478696cce72cf7e5d7d9d46535331d81d97
Carl S. Yestrau Jr <spam@featureblend.com>
2011-05-26 15:41:59 -0700

    Initial lil'nub.

---
+++ beacon.js
@@ -0,0 +1,26 @@
+var n$ = {};
+(function(ns){
+    function Tracker(beacon, options){
+        this.beacon = beacon;
+        this.options = options || {};
+        this.queryPrefix = (this.options.queryPrefix)?"?":"";
+        this.enabled = this.options.hasOwnProperty("enabled")?this.options.enabled:true;
+    }
+    Tracker.prototype.log = function(kv){
+        if(!this.enabled){
+            return;
+        }
+        var params = [];
+        kv._cb = (new Date()).getTime();
+        for(var k in kv){
+            params.push("&");
+            params.push(encodeURIComponent(k));
+            params.push("=");
+            params.push(encodeURIComponent(kv[k]));
+        }
+        var src = this.beacon + this.queryPrefix + params.join("").substr(1);
+        (new Image()).src = src;
+    };
+    ns.Tracker = Tracker;
+})(n$);
+
