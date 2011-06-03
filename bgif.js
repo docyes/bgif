0787af2c954cc0a16e60b4ccd2a359c96b5e6023
Carl S. Yestrau Jr <spam@featureblend.com>
2011-06-03 13:48:54 -0700

    google js linter --strict fixes.

--- bgif.js
+++ bgif.js
@@ -1,43 +1,48 @@
 var n$ = {};
-(function(ns){
-    /**
-     * THAT'S ONE BGIF!
-     *
-     * @param {String} beacon The fully qualified HTTP resource to handle beacon requests.
-     * @param {Object} options
-     *                 @param {Boolean} enabled Defaults to true, disable/enable HTTP requests.
-     *                 @param {Boolean} queryPrefix Defaults to empty false, add or remove the ? prefix from passed beacon string. 
-     */
-    function BGIF(beacon, options){
-        this.b = beacon;
-        this.o = options || {};
-        this.qP = this.o.queryPrefix ? '?' : '';
-        this.e = this.o.hasOwnProperty('enabled') ? this.o.enabled : true;
+(function(ns) {
+  /**
+   * THAT'S ONE BGIF!
+   *
+   * @param {String} beacon The fully qualified HTTP resource to handle
+   *                 beacon requests.
+   * @param {Object} options Optional object literal of options.
+   *                 @param {Boolean} enabled Defaults to true,
+   *                                  disable/enable HTTP requests.
+   *                 @param {Boolean} queryPrefix Defaults to empty false,
+   *                                  add or remove the ? prefix from passed
+   *                                  beacon string.
+   */
+  function BGIF(beacon, options) {
+    this.b = beacon;
+    this.o = options || {};
+    this.qP = this.o.queryPrefix ? '?' : '';
+    this.e = this.o.hasOwnProperty('enabled') ? this.o.enabled : true;
+  }
+  /**
+   * LOG DAT SHIT!
+   *
+   * @param {Object} kv A one-level deep object literal of key/value pairs.
+   *                    Don't worry about escaping BGIF do it!
+   */
+  BGIF.prototype.log = function(kv) {
+    if (!this.e) {
+      return;
     }
-    /**
-     * LOG DAT SHIT!
-     *
-     * @param {Object} kv A one-level deep object literal of key/value pairs. Don't worry about escaping BGIF do it!
-     */
-    BGIF.prototype.log = function(kv){
-        if(!this.e){
-            return;
-        }
-        var s,
-            p = [],
-            e = encodeURIComponent;
-        kv._cb = (new Date()).getTime();
-        for(var k in kv){
-            p.concat([
-                '&',
-                e(k),
-                '=',
-                e(kv[k])
-            ]);
-        }
-        s = this.b + this.qP + p.join('').substr(1);
-        (new Image()).src = s;
-    };
-    ns.BGIF = BGIF;
+    var s,
+        p = [],
+        e = encodeURIComponent;
+    kv._cb = (new Date()).getTime();
+    for (var k in kv) {
+      p.concat([
+        '&',
+        e(k),
+        '=',
+        e(kv[k])
+      ]);
+    }
+    s = this.b + this.qP + p.join('').substr(1);
+    (new Image()).src = s;
+  };
+  ns.BGIF = BGIF;
 })(n$);
 
