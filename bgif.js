4ed9981937e9bc72d313a64c99aeac1e8d224953
Doc Yes <spam@featureblend.com>
2011-06-04 10:50:45 -0700

    Add timeout option for requesting an image, removes from buffer and terminates request.

--- bgif.js
+++ bgif.js
@@ -13,6 +13,9 @@ var n$ = {};
    *                                  wait before making a request.
    *                 @param {Number}  concurrent Defaults to 1, the maximum
    *                                  number of concurrent connections.
+   *                 @param {Number}  timeout Defaults to 250,
+   *                                  how long to wait for a request
+   *                                  before timing out.
    */
   function BGIF(path, options) {
     this.path = path;
@@ -22,6 +25,8 @@ var n$ = {};
     this.defer = this.options.hasOwnPoperty('defer') ? this.options.defer : 0;
     this.concurrent = this.options.hasOwnProperty('concurrent') ?
         this.options.concurrent : 1;
+    this.timeout = this.options.hasOwnProperty('timeout') ? 
+        this.options.timeout : 250
     this.connections = [];
   }
   /**
@@ -39,7 +44,7 @@ var n$ = {};
     }
     var time = (new Date()).getTime();
     var connection = setTimeout(function() {
-      var src, params = [],
+      var src, timeout, that = this, params = [],
           img = new Image();
       kv._time = time;
       for (var k in kv) {
@@ -48,17 +53,25 @@ var n$ = {};
         );
       }
       src = this.path + '?' + params.join('').substr(1);
+      timeout = setTimeout(function() {
+        img = null;
+        that.removeConnection(connection);
+      }, this.timeout);
       img.onload = img.onerror = function() {
-        for (var i = 0, l = this.connections.length; i < l; i++) {
-          if (this.connections[i] == connection) {
-            this.connections.splice(i, 1);
-            break;
-          }
-        }
-      }
+        clearTimeout(timeout);
+        that.removeConnection(connection);
+      };
       img.src = src;
-    }, this.defer);
+   }, this.defer);
     this.connections.push(connection);
+  };
+  BGIF.prototype = removeConnection(connection){
+    for (var i = 0, l = this.connections.length; i < l; i++) {
+      if (this.connections[i] == id) {
+        this.connections.splice(i, 1);
+        break;
+      }
+    }
   };
   ns.BGIF = BGIF;
 })(n$);
